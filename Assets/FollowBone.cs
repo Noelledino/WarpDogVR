7d4ecfd1c77226eb9c2d0b8b21e8e473e8b2153f
Unknown <qq265@nyu.edu>
2017-10-29 20:05:42 -0400

    pick up/come
    pick up fixed, size adjust, pick the bone dog would come

--- Assets/FollowBone.cs
+++ Assets/FollowBone.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowBone : MonoBehaviour {
-
+	
+	public float speed = 1.0f;
+	public bool pickedUp = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,21 @@ public class FollowBone : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		followBone ();
+	}
+
+	public void IsPickedUp(){
+		pickedUp = true;
+	}
+
+	public void IsDetached(){
+		pickedUp = false;
+	}
+	public void followBone(){
+		//transform.position = Vector3.MoveTowards (transform.position,new Vector3(0,1,0), speed*Time.deltaTime);
+		if(pickedUp == true){
+			transform.position = Vector3.Lerp(transform.position, new Vector3(-1.86f, 0.7f, -0.28f), speed * Time.deltaTime);
+		}
+
 	}
 }
